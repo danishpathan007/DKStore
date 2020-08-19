aeca38ad42cfe4bff9bd029d3af3566f4505e7c0
Danish Khan <danishkhan@zapbuild.com>
2020-08-19 18:01:22 +0530

    Category Table cell added

---
+++ DKStore/CategoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCell.swift
+//  DKStore
+//
+//  Created by Danish Khan on 19/08/20.
+//  Copyright © 2020 Danish Khan. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ DKStore/Helper/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  DKStore
+//
+//  Created by Danish Khan on 17/07/20.
+//  Copyright © 2020 Danish Khan. All rights reserved.
+//
+
+import Foundation
+
+//Firebase Headers
+public let kUSER_PATH = "User"
+public let kCATEGORY_PATH = "Category"
+public let kITEMS_PATH = "Items"
+public let kBASKET_PATH = "Basket"
+
+//Category
+public let kNAME = "name"
+public let kIMAGENAME = "imageName"
+public let kOBJECTID = "objectId"
